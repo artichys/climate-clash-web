5978e603d7f79c084c67c1e64ecbead6deb8ebe9
gcarlo11 <ridhorasyid676@gmail.com>
2026-04-18 12:15:22 +0700

    Refactor ResultController class to implement result display and button functionality

--- scripts/scenes/result_controller.gd
+++ scripts/scenes/result_controller.gd
@@ -1,11 +1,22 @@
-extends Node
+extends Control
 
+var run_state: RunState
 
-# Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	run_state = get_node("/root/RunStateNode")
 
+	var title: Label = get_node("Margin/VBox/ResultTitle")
+	var detail: Label = get_node("Margin/VBox/ResultDetail")
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+	title.text = "VICTORY - Neo-Archipelago Saved" if run_state.last_run_won else "GAME OVER"
+	detail.text = run_state.last_run_message if run_state.last_run_message != "" else "Run selesai."
+
+	get_node("Margin/VBox/RestartButton").pressed.connect(_on_restart)
+	get_node("Margin/VBox/MenuButton").pressed.connect(_on_menu)
+
+func _on_restart() -> void:
+	run_state.reset_for_new_run()
+	get_tree().change_scene_to_file("res://scenes/Map.tscn")
+
+func _on_menu() -> void:
+	get_tree().change_scene_to_file("res://scenes/MainMenu.tscn")
