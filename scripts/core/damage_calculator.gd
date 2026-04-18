9393f43fb5b99279180a51b32b7873a17cf30275
gcarlo11 <ridhorasyid676@gmail.com>
2026-04-18 11:55:38 +0700

    Refactor DamageCalculator class to extend RefCounted and implement damage calculation logic

--- scripts/core/damage_calculator.gd
+++ scripts/core/damage_calculator.gd
@@ -1,11 +1,23 @@
-extends Node
+extends RefCounted
+class_name DamageCalculator
 
+static func get_element_multiplier(enemy_type: int, element_type: int) -> float:
+	if enemy_type == GameEnums.EnemyType.FLOOD and element_type == GameEnums.ElementType.WATER:
+		return 1.5
+	if enemy_type == GameEnums.EnemyType.FLOOD and element_type == GameEnums.ElementType.THERMAL:
+		return 0.5
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
+	if enemy_type == GameEnums.EnemyType.HEATWAVE and element_type == GameEnums.ElementType.THERMAL:
+		return 1.5
+	if enemy_type == GameEnums.EnemyType.HEATWAVE and element_type == GameEnums.ElementType.WATER:
+		return 0.5
 
+	return 1.0
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+static func calculate_damage(card: CardData, enemy: EnemyData, offensive_buff: int) -> int:
+	var base_damage := card.damage
+	if card.type == GameEnums.CardType.OFFENSIVE:
+		base_damage += offensive_buff
+
+	var mult := get_element_multiplier(enemy.type, card.element)
+	return int(round(base_damage * mult))
