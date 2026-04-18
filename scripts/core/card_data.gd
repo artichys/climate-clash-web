b1020e09b3c1d373d537db062dfb5b7e15c9bf51
gcarlo11 <ridhorasyid676@gmail.com>
2026-04-18 11:52:51 +0700

    Refactor CardData class to extend RefCounted and define card properties

--- scripts/core/card_data.gd
+++ scripts/core/card_data.gd
@@ -1,11 +1,43 @@
-extends Node
+extends RefCounted
+class_name CardData
 
+var id: String = ""
+var display_name: String = ""
+var type: int = GameEnums.CardType.OFFENSIVE
+var element: int = GameEnums.ElementType.NEUTRAL
+var cost: int = 1
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
+var damage: int = 0
+var block: int = 0
+var heal: int = 0
+var meter_delta: int = 0
 
+var draw_now: int = 0
+var draw_next_turn: int = 0
+var heal_next_turn: int = 0
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+var offensive_buff: int = 0
+var reduce_all_costs_this_turn: bool = false
+var suppress_enemy_meter_gain_turns: int = 0
+
+var exhaust: bool = false
+
+func clone() -> CardData:
+	var c := CardData.new()
+	c.id = id
+	c.display_name = display_name
+	c.type = type
+	c.element = element
+	c.cost = cost
+	c.damage = damage
+	c.block = block
+	c.heal = heal
+	c.meter_delta = meter_delta
+	c.draw_now = draw_now
+	c.draw_next_turn = draw_next_turn
+	c.heal_next_turn = heal_next_turn
+	c.offensive_buff = offensive_buff
+	c.reduce_all_costs_this_turn = reduce_all_costs_this_turn
+	c.suppress_enemy_meter_gain_turns = suppress_enemy_meter_gain_turns
+	c.exhaust = exhaust
+	return c
