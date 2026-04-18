f00719a3398c49319a866d0c5316f28abc190ca3
gcarlo11 <ridhorasyid676@gmail.com>
2026-04-18 11:55:25 +0700

    Refactor EnemyData class to extend RefCounted and define enemy properties

--- scripts/core/enemy_data.gd
+++ scripts/core/enemy_data.gd
@@ -1,11 +1,18 @@
-extends Node
+extends RefCounted
+class_name EnemyData
 
+var id: String = ""
+var display_name: String = ""
+var type: int = GameEnums.EnemyType.FLOOD
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
+var max_hp: int = 1
+var base_attack: int = 1
+var meter_per_turn: int = 1
 
+var attack_scale_every_turns: int = 0
+var attack_scale_amount: int = 0
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+var has_phase_two: bool = false
+var phase_two_threshold_percent: int = 50
+var phase_two_meter_per_turn: int = 1
+var phase_two_attack_bonus: int = 0
