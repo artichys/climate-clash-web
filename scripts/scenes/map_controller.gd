72f4291e9f7fb2a68860d100d876b609d232e15e
gcarlo11 <ridhorasyid676@gmail.com>
2026-04-18 12:15:28 +0700

    Refactor MapController class to implement UI elements and node progress display

--- scripts/scenes/map_controller.gd
+++ scripts/scenes/map_controller.gd
@@ -1,11 +1,55 @@
-extends Node
+extends Control
 
+var run_state: RunState
+var info_label: Label
+var node_list_label: Label
 
-# Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	run_state = get_node("/root/RunStateNode")
+	info_label = get_node("Margin/VBox/InfoLabel")
+	node_list_label = get_node("Margin/VBox/NodeListLabel")
 
+	get_node("Margin/VBox/ContinueButton").pressed.connect(_on_continue_pressed)
+	get_node("Margin/VBox/BackButton").pressed.connect(_on_back_pressed)
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+	_refresh_view()
+
+func _refresh_view() -> void:
+	info_label.text = "HP: %d/%d | Temp: %d/%d | Deck: %d" % [
+		run_state.current_hp,
+		run_state.max_hp,
+		run_state.temperature,
+		run_state.temperature_max,
+		run_state.deck_card_ids.size()
+	]
+
+	var text := "Node Progress:\n"
+	for node in GameDatabase.get_run_nodes():
+		var marker := ">>" if node.index - 1 == run_state.current_node_index else "  "
+		var label := "Unknown"
+		if node.node_type == GameEnums.NodeType.BATTLE:
+			label = "Battle - %s" % str(node.enemy_kind)
+		elif node.node_type == GameEnums.NodeType.EVENT:
+			label = "Mini Event - Sanctuary of the Green"
+		elif node.node_type == GameEnums.NodeType.BOSS:
+			label = "Final Boss - Climate Collapse"
+
+		text += "%s Node %d: %s\n" % [marker, node.index, label]
+
+	node_list_label.text = text
+
+func _on_continue_pressed() -> void:
+	var node := run_state.get_current_node()
+	if node == null:
+		if run_state.last_run_message == "":
+			run_state.mark_run_win("Semua node selesai.")
+		get_tree().change_scene_to_file("res://scenes/Result.tscn")
+		return
+
+	if node.node_type == GameEnums.NodeType.EVENT:
+		get_tree().change_scene_to_file("res://scenes/Event.tscn")
+	else:
+		get_tree().change_scene_to_file("res://scenes/Battle.tscn")
+
+func _on_back_pressed() -> void:
+	get_tree().change_scene_to_file("res://scenes/MainMenu.tscn")
